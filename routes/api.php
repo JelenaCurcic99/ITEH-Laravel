ea2b9e2230d21a53710b79a38b37606f1a7b9571
JelenaCurcic99 <jelenacurciccc@gmail.com>
2022-01-13 20:43:19 +0100

    REST API, 6 ruta definisano 3 tipa GET,PUT,DELETE za Kurir i Primalac modele

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\KurirController;
+use App\Http\Controllers\PrimalacController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('kurir', [KurirController::class, 'index']);
+Route::get('kurir/{kurir}', [KurirController::class, 'show']);
+Route::put('kurir/{kurir}', [KurirController::class, 'update']);
+Route::delete('kurir/{kurir}', [KurirController::class, 'destroy']);
+
+Route::get('primalac', [PrimalacController::class, 'index']);
+Route::delete('primalac/{primalac}', [PrimalacController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
